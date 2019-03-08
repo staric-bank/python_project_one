02b29dcfd6c5f13bd115749b6a276afef859e4b5
Eric Escalante <eric.m.escalante@gmail.com>
2019-03-08 11:36:40 -0600

    updated checkbook.py

--- checkbook.py
+++ checkbook.py
@@ -6,9 +6,33 @@ current_balance = 0.00
 
 
 print(' ~~~ Welcome to your terminal checkbook! ~~~ ')
-print('\n')
-print('What would you like to do?')
+
+def user_options():
+    print('\nWhat would you like to do? Please select an option')
+
+    prompt = int(input('''\n1) view current balance\n'''+'''2) record a debit (withdrawl)\n'''+'''3) record a credit (deposit)\n'''+'''4) exit\n'''))
+    print('Your choice: {}'.format(prompt))
+
+    # if prompt==1:
+    #     user=current_balance()
+    # elif prompt==2:
+    #     user=debit_balance()
+    # elif prompt==3:
+    #     user=credit_balance()
+    # elif prompt==4:
+    #     user=exit()
+    # else:
+    #     print('Invalid choice: {}'.format(prompt))
+    #     user_options()
 
 
+# def debit_balance(current_balance, amount):
+#     print('How much is the debit?')
+#     if current_balance - amount < 0:
+#         print('Non-sufficient funds')
+#     else:
+#         current_balance -= amount
+#     return current_balance
 
-print('Thanks, have a great day!')
+user_options()
+# print('Thanks, have a great day!')
