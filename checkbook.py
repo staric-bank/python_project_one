4b23abbb3bcee3556284126b8dc9a44e17326ab4
Eric Escalante <eric.m.escalante@gmail.com>
2019-03-08 12:06:29 -0600

    working with global varible

--- checkbook.py
+++ checkbook.py
@@ -3,8 +3,6 @@
 
 current_balance = 0.00
 
-
-
 print(' ~~~ Welcome to your terminal checkbook! ~~~ ')
 
 def user_options():
@@ -13,26 +11,32 @@ def user_options():
     prompt = int(input('''\n1) view current balance\n'''+'''2) record a debit (withdrawl)\n'''+'''3) record a credit (deposit)\n'''+'''4) exit\n'''))
     print('Your choice: {}'.format(prompt))
 
-    # if prompt==1:
-    #     user=current_balance()
-    # elif prompt==2:
-    #     user=debit_balance()
-    # elif prompt==3:
-    #     user=credit_balance()
-    # elif prompt==4:
-    #     user=exit()
-    # else:
-    #     print('Invalid choice: {}'.format(prompt))
-    #     user_options()
-
-
-def debit_balance(amount):
-    amount = float(input('How much is the debit: $'))
-    current_balance -= amount
-    print("Your new account balance is %.2f" % current_balance)
+    if prompt==1:
+        print('Your current balance is $%.2f' % current_balance)
+    elif prompt==2:
+        user=debit_balance()
+    elif prompt==3:
+        user=credit_balance()
+    elif prompt==4:
+        print('\nThanks, have a great day!')
+    else:
+        print('Invalid choice: {}'.format(prompt))
+        user_options()
     user_options()
 
-def credit_balance(amount):
+
+def debit_balance():
+    amount = float(input('How much is the debit: $'))
+    # global current_balance - amount
+    print('\nYour current balance is $%.2f' % current_balance)
+    user_options()
+
+def credit_balance():
     amount = float(input('How much is the credit: $'))
+    # global current_balance + amount
+    print('\nYour current balance is $%.2f' % current_balance)
+    user_options()
+
+
 user_options()
 # print('Thanks, have a great day!')
