385008ddabcf38540c5cb36427e0ef7a125bdd6c
Eric Escalante <eric.m.escalante@gmail.com>
2019-03-08 10:37:24 -0600

    updated with current balance variable

--- checkbook.py
+++ checkbook.py
@@ -1,4 +1,14 @@
 # Python Project
 # Command Line Checkbook Application
 
+current_balance = 0.00
+
+
+
 print(' ~~~ Welcome to your terminal checkbook! ~~~ ')
+print('\n')
+print('What would you like to do?')
+
+
+
+print('Thanks, have a great day!')
